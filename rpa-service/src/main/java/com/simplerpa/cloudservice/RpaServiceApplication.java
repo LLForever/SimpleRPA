cb98ee6798f37060db00e5497debc8443e410ec6
Rui Chen <346904028@qq.com>
2022-04-07 21:41:11 +0800

    feat: 构建后端代码框架

---
+++ rpa-service/src/main/java/com/simplerpa/cloudservice/RpaServiceApplication.java
@@ -0,0 +1,17 @@
+package com.simplerpa.cloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: TODO
+ * @author: ChenRui98
+ * @date: 2022年04月07日 21:01
+ */
+
+@SpringBootApplication
+public class RpaServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(RpaServiceApplication.class, args);
+    }
+}
---
+++ rpa-service/src/main/java/com/simplerpa/cloudservice/controller/PanelTaskController.java
@@ -0,0 +1,15 @@
+package com.simplerpa.cloudservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: TODO
+ * @author: ChenRui98
+ * @date: 2022年04月07日 21:34
+ */
+
+@RestController
+@RequestMapping("/panel_task")
+public class PanelTaskController {
+}
